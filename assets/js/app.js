31ca3e4de449dff757c90720e32e4799f22dc1c4
Manuel_Perez_Reyes <manuel.p.r@hotmail.com>
2025-09-30 23:15:28 -0400

    Se esta barajando el deck y se arreglo un problema que estaba dando la libreria

---
+++ assets/js/app.js
@@ -0,0 +1,28 @@
+const cartasJugador = document.getElementById('jugador-cartas'),
+    cartasMaquina = document.getElementById('computador-cartas');
+
+let deck = [];
+
+const types = ['C', 'D', 'H', 'S'];
+const letters = ['A', 'J', 'Q', 'K'];
+
+const createDeck = () => {
+    for (let i = 2; i <= 10; i++) {
+        for (const type of types) {
+            deck.push(i + type);
+        }
+
+    }
+
+    for (const letter of letters) {
+        for (const type of types) {
+            deck.push(letter + type);
+        }
+    }
+    
+    deck = _.shuffle(deck)
+
+    console.log(deck);
+}
+
+createDeck()

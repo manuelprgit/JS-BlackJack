640f8e1b6af84c20cbc0a54237ea782122c860c3
Manuel_Perez_Reyes <manuel.p.r@hotmail.com>
2025-10-03 00:11:44 -0400

    Add game logic and UI updates for Black Jack functionality

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,15 @@
 const cartasJugador = document.getElementById('jugador-cartas'),
-    cartasMaquina = document.getElementById('computador-cartas');
+      cartasMaquina = document.getElementById('computador-cartas'),
+      getCard = document.getElementById('getCard'),
+      playerPointsContent = document.getElementById('playerPointsContent'),
+      pcPointsContent = document.getElementById('pcPointsContent'),
+      stopGame = document.getElementById('stopGame');
+
 
 let deck = [];
+
+let playerPoints = 0;
+let pcPoints = 0;
 
 const types = ['C', 'D', 'H', 'S'];
 const letters = ['A', 'J', 'Q', 'K'];
@@ -20,8 +28,7 @@ const createDeck = () => {
         }
     }
 
-    deck = _.shuffle(deck);
-    console.log(deck);
+    deck = _.shuffle(deck); 
 }
 
 createDeck()
@@ -35,8 +42,49 @@ const cardValue = (card) => {
     const value = card.substring(0, card.length - 1)
     return isNaN(value) 
         ? value === 'A' ? 11 : 10
-        : points = Number(value);
+        : Number(value);
 }
 
-console.log(cardValue(deck[0]))
+const createCard = (card, cardContainer) => {
+    const imgCard = document.createElement('img');
+    imgCard.src = `/assets/cartas/${card}.png`
+    imgCard.className = 'carta';
+    cardContainer.append(imgCard);
+}
+
+const PCTurn = (minPoints) => {
+    do{ 
+        const card = takeCard();
+        const value = cardValue(card);
+        createCard(card, cartasMaquina);
+        pcPoints += value;
+        pcPointsContent.textContent = pcPoints;
+        if(minPoints > 21) break;
+    }
+    while(pcPoints < minPoints && pcPoints < 21);
+}
+
+getCard.addEventListener('click', e=> {
+    const card = takeCard()
+    const value = cardValue(card);
+    playerPoints += value;
+    playerPointsContent.textContent = playerPoints;
+    createCard(card, cartasJugador);
+
+    if(playerPoints > 21){
+        console.warn('Perdiste');
+        getCard.disabled = true;
+        PCTurn(playerPoints)
+    }else if(playerPoints === 21){
+        console.warn('21, Genial!')
+        getCard.disabled = true;
+        stopGame.disabled = true;
+    }
+})
+
+stopGame.addEventListener('click', e=>{
+    getCard.disabled = true;
+    stopGame.disabled = true;
+    PCTurn(playerPoints);
+})
 

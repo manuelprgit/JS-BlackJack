a57e5980d7abc007313e667472552d52d006e006
Manuel_Perez_Reyes <manuel.p.r@hotmail.com>
2025-10-11 16:07:54 -0400

    Refactor app.js to encapsulate game logic within an IIFE and improve code organization

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,124 +1,129 @@
-const cartasJugador = document.getElementById('jugador-cartas'),
-    cartasMaquina = document.getElementById('computador-cartas'),
-    getCard = document.getElementById('getCard'),
-    playerPointsContent = document.getElementById('playerPointsContent'),
-    pcPointsContent = document.getElementById('pcPointsContent'),
-    stopGame = document.getElementById('stopGame');
-
-
-let deck = [];
-
-let playerPoints = 0;
-let pcPoints = 0;
-
-const types = ['C', 'D', 'H', 'S'];
-const letters = ['A', 'J', 'Q', 'K'];
-
-const createDeck = () => {
-    for (let i = 2; i <= 10; i++) {
-        for (const type of types) {
-            deck.push(i + type);
+(async () => {
+    // 'use strict';
+    const cartasJugador = document.getElementById('jugador-cartas'),
+        cartasMaquina = document.getElementById('computador-cartas'),
+        getCard = document.getElementById('getCard'),
+        playerPointsContent = document.getElementById('playerPointsContent'),
+        pcPointsContent = document.getElementById('pcPointsContent'),
+        stopGame = document.getElementById('stopGame');
+    
+    
+    let deck = [];
+    // deck = 1;
+    
+    let playerPoints = 0;
+    let pcPoints = 0;
+    
+    const types = ['C', 'D', 'H', 'S'];
+    const letters = ['A', 'J', 'Q', 'K'];
+    
+    const createDeck = () => {
+        for (let i = 2; i <= 10; i++) {
+            for (const type of types) {
+                deck.push(i + type);
+            }
+    
         }
-
+    
+        for (const letter of letters) {
+            for (const type of types) {
+                deck.push(letter + type);
+            }
+        }
+    
+        deck = _.shuffle(deck);
+        console.log(deck);
     }
-
-    for (const letter of letters) {
-        for (const type of types) {
-            deck.push(letter + type);
+    
+    createDeck()
+    
+    const takeACard = () => {
+        if (deck.length === 0) throw 'No hay mas cartas';
+        return deck.pop();
+    }
+    
+    const cardValue = (card) => {
+        const value = card.substring(0, card.length - 1)
+        return isNaN(value)
+            ? value === 'A' ? 11 : 10
+            : Number(value);
+    }
+    
+    const createCard = (card, cardContainer) => {
+        const imgCard = document.createElement('img');
+        imgCard.src = `/assets/cartas/${card}.png`
+        imgCard.className = 'carta';
+        cardContainer.append(imgCard);
+    }
+    
+    const PCTurn = () => {
+        do {
+            const card = takeACard();
+            const value = cardValue(card);
+            createCard(card, cartasMaquina);
+            pcPoints += value;
+            pcPointsContent.textContent = pcPoints;
+            if (playerPoints > 21) break;
+        }
+        while (pcPoints <= playerPoints && pcPoints < 21);
+        if (pcPoints <= 21 && pcPoints > playerPoints) {
+            setTimeout(() => {
+                alert('La computadora ganó!')
+            }, 100);
+        } else if (pcPoints > 21) {
+            setTimeout(() => {
+                alert('Jugador gana!')
+            }, 100);
         }
     }
-
-    deck = _.shuffle(deck);
-    console.log(deck);
-}
-
-createDeck()
-
-const takeACard = () => {
-    if (deck.length === 0) throw 'No hay mas cartas';
-    return deck.pop();
-}
-
-const cardValue = (card) => {
-    const value = card.substring(0, card.length - 1)
-    return isNaN(value)
-        ? value === 'A' ? 11 : 10
-        : Number(value);
-}
-
-const createCard = (card, cardContainer) => {
-    const imgCard = document.createElement('img');
-    imgCard.src = `/assets/cartas/${card}.png`
-    imgCard.className = 'carta';
-    cardContainer.append(imgCard);
-}
-
-const PCTurn = () => {
-    do {
-        const card = takeACard();
+    
+    const resetGame = () => {
+        deck = [];
+        createDeck();
+        cartasJugador.textContent = '';
+        cartasMaquina.textContent = '';
+        playerPointsContent.textContent = '0';
+        pcPointsContent.textContent = '0';
+        getCard.disabled = false;
+        stopGame.disabled = false;
+        playerPoints = 0;
+        pcPoints = 0;
+    }
+    
+    getCard.addEventListener('click', e => {
+        const card = takeACard()
         const value = cardValue(card);
-        createCard(card, cartasMaquina);
-        pcPoints += value;
-        pcPointsContent.textContent = pcPoints;
-        if (playerPoints > 21) break;
-    }
-    while (pcPoints <= playerPoints && pcPoints < 21);
-    if (pcPoints <= 21 && pcPoints > playerPoints) {
-        setTimeout(() => {
-            alert('La computadora ganó!')
-        }, 100);
-    } else if (pcPoints > 21) {
-        setTimeout(() => {
-            alert('Jugador gana!')
-        }, 100);
-    }
-}
-
-const resetGame = () => {
-    deck = [];
-    createDeck();
-    cartasJugador.textContent = '';
-    cartasMaquina.textContent = '';
-    playerPointsContent.textContent = '0';
-    pcPointsContent.textContent = '0';
-    getCard.disabled = false;
-    stopGame.disabled = false;
-    playerPoints = 0;
-    pcPoints = 0;
-}
-
-getCard.addEventListener('click', e => {
-    const card = takeACard()
-    const value = cardValue(card);
-    playerPoints += value;
-    playerPointsContent.textContent = playerPoints;
-    createCard(card, cartasJugador);
-
-    if (playerPoints > 21) {
-        getCard.disabled = true;
-        PCTurn(playerPoints);
-        setTimeout(() => {
-            alert('Perdiste');
-        }, 100);
-    } else if (playerPoints === 21) {
+        playerPoints += value;
+        playerPointsContent.textContent = playerPoints;
+        createCard(card, cartasJugador);
+    
+        if (playerPoints > 21) {
+            getCard.disabled = true;
+            PCTurn(playerPoints);
+            setTimeout(() => {
+                alert('Perdiste');
+            }, 100);
+        } else if (playerPoints === 21) {
+            getCard.disabled = true;
+            stopGame.disabled = true;
+            setTimeout(() => {
+                alert('21, Genial!');
+            }, 100);
+        } else if (pcPoints === playerPoints) {
+            setTimeout(() => {
+                alert('Nadie gana');
+            }, 100);
+        }
+    })
+    
+    stopGame.addEventListener('click', e => {
         getCard.disabled = true;
         stopGame.disabled = true;
-        setTimeout(() => {
-            alert('21, Genial!');
-        }, 100);
-    } else if (pcPoints === playerPoints) {
-        setTimeout(() => {
-            alert('Nadie gana');
-        }, 100);
-    }
-})
-
-stopGame.addEventListener('click', e => {
-    getCard.disabled = true;
-    stopGame.disabled = true;
-    PCTurn(playerPoints);
-})
-
-newGame.addEventListener('click', e => {
-    resetGame();
-})
+        PCTurn(playerPoints);
+    })
+    
+    newGame.addEventListener('click', e => {
+        resetGame();
+    })
+    
+})();

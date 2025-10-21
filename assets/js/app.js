9e7992c072454680a3e206567da3f3220dab69ba
Manuel_Perez_Reyes <manuel.p.r@hotmail.com>
2025-10-21 09:44:23 -0400

    Refactor game logic to improve card handling and point accumulation

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,69 +5,75 @@
           getCard             = document.getElementById('getCard'),
           playerPointsContent = document.getElementById('playerPointsContent'),
           pcPointsContent     = document.getElementById('pcPointsContent'),
-          stopGame            = document.getElementById('stopGame');
-        
+          stopGame            = document.getElementById('stopGame'),
+          playersPointSpan    = document.querySelectorAll('span');
+
     let deck = [];
     const playersPoints = [];
-    
+
     const types = ['C', 'D', 'H', 'S'],
-          letters = ['A', 'J', 'Q', 'K'];
+        letters = ['A', 'J', 'Q', 'K'];
 
     const initGame = (players = 2) => {
         deck = createDeck();
-        for(let i = 0; i < players; i++){
+        for (let i = 0; i < players; i++) {
             playersPoints.push(0)
         }
-        console.log({playersPoints})
     }
-    
+
     const createDeck = () => {
         for (let i = 2; i <= 10; i++) {
             for (const type of types) {
                 deck.push(i + type);
             }
-    
+
         }
-    
+
         for (const letter of letters) {
             for (const type of types) {
                 deck.push(letter + type);
             }
         }
-    
-        return _.shuffle(deck);        
+
+        return _.shuffle(deck);
     }
-    
+
     const takeACard = () => {
         if (deck.length === 0) throw 'No hay mas cartas';
         return deck.pop();
     }
-    
+
     const cardValue = (card) => {
         const value = card.substring(0, card.length - 1)
         return isNaN(value)
             ? value === 'A' ? 11 : 10
             : Number(value);
     }
-    
+
     const createCard = (card, cardContainer) => {
         const imgCard = document.createElement('img');
         imgCard.src = `/assets/cartas/${card}.png`
         imgCard.className = 'carta';
         cardContainer.append(imgCard);
     }
-    
-    const PCTurn = () => {
+
+    const accumulatePoints = (card, turn) => {
+        playersPoints[turn] += cardValue(card);
+        playersPointSpan[turn].textContent = playersPoints[turn];
+    }
+
+    const PCTurn = (minPoints) => {
         do {
             const card = takeACard();
-            const value = cardValue(card);
+
+            accumulatePoints(card, playersPoints.length - 1);
+
             createCard(card, cartasMaquina);
-            pcPoints += value;
             pcPointsContent.textContent = pcPoints;
-            if (playerPoints > 21) break;
+            if (minPoints > 21) break;
         }
-        while (pcPoints <= playerPoints && pcPoints < 21);
-        if (pcPoints <= 21 && pcPoints > playerPoints) {
+        while (pcPoints <= minPoints && pcPoints < 21);
+        if (pcPoints <= 21 && pcPoints > minPoints) {
             setTimeout(() => {
                 alert('La computadora ganó!')
             }, 100);
@@ -77,7 +83,7 @@
             }, 100);
         }
     }
-    
+
     const resetGame = () => {
         deck = [];
         createDeck();
@@ -90,14 +96,14 @@
         playerPoints = 0;
         pcPoints = 0;
     }
-    
+
     getCard.addEventListener('click', e => {
         const card = takeACard()
         const value = cardValue(card);
         playerPoints += value;
         playerPointsContent.textContent = playerPoints;
         createCard(card, cartasJugador);
-    
+
         if (playerPoints > 21) {
             getCard.disabled = true;
             PCTurn(playerPoints);
@@ -116,13 +122,13 @@
             }, 100);
         }
     })
-    
+
     stopGame.addEventListener('click', e => {
         getCard.disabled = true;
         stopGame.disabled = true;
         PCTurn(playerPoints);
     })
-    
+
     newGame.addEventListener('click', e => {
         resetGame();
     })

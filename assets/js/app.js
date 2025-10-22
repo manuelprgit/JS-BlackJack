7cfa86d78c9de14e2d74e6a6e3ecf6e6db0e6e62
Manuel_Perez_Reyes <manuel.p.r@hotmail.com>
2025-10-22 05:21:28 -0400

    Refactor card container elements to use a common class for improved styling and consistency

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,6 @@
 (async () => {
     // 'use strict';
-    const cartasJugador       = document.getElementById('jugador-cartas'),
-          cartasMaquina       = document.getElementById('computador-cartas'),
+    const playersCardContent  = document.querySelectorAll('.card-content'),
           getCard             = document.getElementById('getCard'),
           playerPointsContent = document.getElementById('playerPointsContent'),
           pcPointsContent     = document.getElementById('pcPointsContent'),
@@ -11,8 +10,8 @@
     let deck = [];
     const playersPoints = [];
 
-    const types = ['C', 'D', 'H', 'S'],
-        letters = ['A', 'J', 'Q', 'K'];
+    const types   = ['C', 'D', 'H', 'S'],
+          letters = ['A', 'J', 'Q', 'K'];
 
     const initGame = (players = 2) => {
         deck = createDeck();
@@ -50,26 +49,31 @@
             : Number(value);
     }
 
-    const createCard = (card, cardContainer) => {
+    const createCard = (card, turn) => {
         const imgCard = document.createElement('img');
         imgCard.src = `/assets/cartas/${card}.png`
         imgCard.className = 'carta';
-        cardContainer.append(imgCard);
+        playersCardContent[turn].append(imgCard);
     }
 
     const accumulatePoints = (card, turn) => {
         playersPoints[turn] += cardValue(card);
         playersPointSpan[turn].textContent = playersPoints[turn];
+        return playersPoints[turn];
     }
 
-    const PCTurn = (minPoints) => {
+    const pCTurn = (minPoints) => {
+        let pcPoints = 0;
+        const pCTurnNumber = playersPoints.length - 1;
         do {
             const card = takeACard();
 
-            accumulatePoints(card, playersPoints.length - 1);
+            pcPoints = accumulatePoints(card, pCTurnNumber);
+            
+            console.log(minPoints);
 
-            createCard(card, cartasMaquina);
-            pcPointsContent.textContent = pcPoints;
+            createCard(card, pCTurnNumber);
+            
             if (minPoints > 21) break;
         }
         while (pcPoints <= minPoints && pcPoints < 21);
@@ -86,9 +90,9 @@
 
     const resetGame = () => {
         deck = [];
-        createDeck();
-        cartasJugador.textContent = '';
-        cartasMaquina.textContent = '';
+        deck = createDeck();
+        // cartasJugador.textContent = '';
+        // cartasMaquina.textContent = '';
         playerPointsContent.textContent = '0';
         pcPointsContent.textContent = '0';
         getCard.disabled = false;
@@ -98,15 +102,14 @@
     }
 
     getCard.addEventListener('click', e => {
-        const card = takeACard()
-        const value = cardValue(card);
-        playerPoints += value;
+        const card = takeACard() 
+        let playerPoints = accumulatePoints(card, 0);
         playerPointsContent.textContent = playerPoints;
-        createCard(card, cartasJugador);
+        createCard(card, 0);
 
         if (playerPoints > 21) {
             getCard.disabled = true;
-            PCTurn(playerPoints);
+            pCTurn(playerPoints);
             setTimeout(() => {
                 alert('Perdiste');
             }, 100);
@@ -126,7 +129,7 @@
     stopGame.addEventListener('click', e => {
         getCard.disabled = true;
         stopGame.disabled = true;
-        PCTurn(playerPoints);
+        pCTurn(playersPoints[0]);
     })
 
     newGame.addEventListener('click', e => {
